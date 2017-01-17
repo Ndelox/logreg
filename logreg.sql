afc01155837f0d4f4b395700891cc0505292312d
Mualim Gendeng <mualim09@gmail>
2017-01-17 23:27:03 +0700

    Create logreg.sql

---
+++ logreg.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `anggota` (
+`nama` VARCHAR( 50 ) NOT NULL ,
+`email` VARCHAR( 25 ) NOT NULL ,
+`userid` VARCHAR( 25 ) NOT NULL ,
+`password` VARCHAR( 25 ) NOT NULL ,
+PRIMARY KEY ( `userid` )
+);
