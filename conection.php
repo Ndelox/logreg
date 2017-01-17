f3699827fb3af69484dcafecbcc89517042ca9f7
Mualim Gendeng <mualim09@gmail>
2017-01-17 23:18:07 +0700

    Create conection.php

---
+++ conection.php
@@ -0,0 +1,9 @@
+# logreg
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root'; 
+$dbpass = ''; 
+$dbname = 'logreg';  
+$konek = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Koneksi Gagal!');
+mysql_select_db($dbname);
+?>
